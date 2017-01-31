66c6c3c6362cf03f3d5d612b213a7db15557aa03
lavoiee <ericjohnlavoie@gmail.com>
2017-01-30 22:56:50 -0500

    Republishing after rebuilding my local Repo

---
+++ StockTrader/StockTrader/Program.cs
@@ -0,0 +1,151 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+using System.Collections; // BitArray
+using System.Net.Sockets; //JSON QUANDL API Bring the data in Figure it out.
+//using System.Net.WebSockets;
+//using System.Net.Http;
+
+//
+// Experiment with Windows Forms Applications. And SQL Server.
+// Locate a Scientific Graphing Library for graphing stock data.
+// Can you use d3.js in C#? ILNumerics SVG Graphic math library.
+namespace StockTrader
+{
+    class Program
+    {
+        //TcpListener myfirstTcpListener = new TcpListener();
+        //Thread myFirstThread = new Thread();
+        //BitArray myBitArray = new BitArray();
+        static Stock msft = new Stock("Microsoft", 65.78m, "MSFT");
+        static Stock apple = new Stock("Apple", 121.95m, "AAPL");
+        static Stock ford = new Stock("Ford", 12.49m, "F");
+        static List<Stock> stocks = new List<Stock>();
+
+        static bool exit = false;
+
+        static void Main(string[] args)
+        {
+            InitializeList();                   
+            while (!exit)
+            {
+                DisplayMenu();                
+            }
+            
+        }
+
+        private static void DisplayTradeModule()
+        {
+            
+        }
+
+        private static void InitializeList()
+        {
+            stocks.Add(msft);
+            stocks.Add(apple);
+            stocks.Add(ford);
+        }
+
+        private static void DisplayCompaniesModule()
+        {
+            Console.WriteLine();
+            Console.WriteLine("Displaying records...");
+            Console.WriteLine();
+            foreach (Stock stock in stocks)
+            {                
+                Console.WriteLine($"Company name:{stock.GetCompany()}");
+                Console.WriteLine($"Price per share:{stock.GetPriceAsString()}");
+                Console.WriteLine($"Ticker symbol:{stock.GetTickerSymbol()}");
+                Console.WriteLine();
+            }
+
+        }
+
+        private static void DisplayMenu()
+        {
+            ConsoleKey keypress;
+            DisplayHeader();
+            DisplayMenuBar();
+            
+
+            keypress = Console.ReadKey().Key;
+
+            DisplayHeader();
+            DisplayMenuBar();
+
+            switch (keypress)
+            {
+                case ConsoleKey.Escape:
+                    exit = true;
+                    break;
+                case ConsoleKey.V:
+                    DisplayCompaniesModule();
+                    break;
+                case ConsoleKey.T:
+                    DisplayTradeModule();
+                    break;
+                case ConsoleKey.R:
+                    DisplaySimulationModule();
+                    break;
+                case ConsoleKey.H:
+                    DisplayHelpModule();
+                    break;
+                default:
+                    DisplayValidFeedback();
+                    break;
+            }
+            //DisplayContinuePrompt();
+        }
+
+        private static void DisplayMenuBar()
+        {
+
+            ColorizeTextGreen("[ESC]", "Escape ");
+            ColorizeTextGreen("[V]", "iew Companies ");
+            ColorizeTextGreen("[T]", "rade ");
+            ColorizeTextGreen("[R]", "un Simulation ");
+            ColorizeTextGreen("[H]", "elp");
+            Console.WriteLine();
+        }
+
+        private static void ColorizeTextGreen(string text, string normal)
+        {
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.Write(text);
+            Console.ResetColor();
+            Console.Write(normal);
+        }
+
+        private static void DisplayValidFeedback()
+        {
+            
+        }
+
+        private static void DisplayHelpModule()
+        {
+            
+        }
+
+        private static void DisplaySimulationModule()
+        {
+            
+        }
+
+        private static void DisplayContinuePrompt()
+        {
+            Console.WriteLine();
+            Console.WriteLine("Press any key to continue...");
+            Console.ReadKey();          
+        }
+
+        private static void DisplayHeader()
+        {
+            Console.Clear();
+            Console.WriteLine();
+            Console.WriteLine("************************STOCK TRADER************************");
+        }
+    }
+}
---
+++ StockTrader/StockTrader/Stock.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StockTrader
+{
+    class Stock
+    {
+        private string company;
+        private decimal sharePrice;
+        private string tickerSymbol;
+
+        public string Company
+        {
+            get { return company; }
+            set { company = value; }
+        }
+
+        public decimal SharePrice
+        {
+            get { return sharePrice; }
+            set { sharePrice = value; }
+        }
+        
+        public string TickerSymbol
+        {
+            get { return tickerSymbol; }
+            set { tickerSymbol = value; }
+        }
+
+        //
+        // Constructors
+        //
+        public Stock()
+        {
+
+        }
+
+        public Stock(string company)
+        {
+            this.company = company;
+        }
+
+        public Stock(string company, decimal sharePrice)
+        {
+            this.company = company;
+            this.sharePrice = sharePrice;
+        }
+
+        public Stock(string company, decimal sharePrice, string tickerSymbol)
+        {
+            this.company = company;
+            this.sharePrice = sharePrice;
+            this.tickerSymbol = tickerSymbol;
+        }
+
+
+        //
+        // Methods
+        //
+        public string GetCompany()
+        {
+            return this.company;
+        }
+
+        public decimal GetSharePrice()
+        {
+            return this.sharePrice;
+        }
+
+        public string GetPriceAsString()
+        {
+            string price = this.sharePrice.ToString();
+            price = String.Format("{0:0.00}", price);
+            return price;
+        }
+
+        public string GetTickerSymbol()
+        {
+            return this.tickerSymbol;
+        }
+    }
+}
